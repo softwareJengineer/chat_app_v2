8b058858d7301584f6dce8307bb4f834e6c37bbb
amurphy99 <amurphy62299@gmail.com>
2025-04-17 12:06:08 -0500

    argument to not use text-to-speech + formatting

--- backend/dementia_chat/services/asr.py
+++ backend/dementia_chat/services/asr.py
@@ -6,35 +6,48 @@ import csv
 import time
 import datetime
 
-
+# Imports
 import config as cf
 import dementia_chat.services.tts as tts
 
-# set API keys
-speech_key, service_region = cf.speech_key, cf.service_region
-speech_config = cf.speech_config
-audio_config = cf.audio_config
+# =======================================================================
+# Set API Keys & Logger
+# =======================================================================
+# Speech configuration
+speech_key,    service_region = cf.speech_key,    cf.service_region
+speech_config, audio_config   = cf.speech_config, cf.audio_config
 
-# set logger
+# Logging
 logger = cf.logging.getLogger("__asr__")
 
-
+# =======================================================================
+# 
+# =======================================================================
 def record_chat():
+    # Restart or append to current
     mode = "w" if cf.script_check == 1 else "a"
+
     with open(cf.script_path, encoding="utf-8-sig", newline='', mode=mode) as f:
         writer = csv.writer(f)
-        cf.chat_history.sort(key=lambda x: x['Time'])
 
-        if cf.script_check == 1:
-            writer.writerow(['Speaker', 'Utt', 'Time'])
+        # Write headers if needed
+        if cf.script_check == 1: writer.writerow(['Speaker', 'Utt', 'Time'])
+        
+        # Make sure the chat history is in chronological order & write the most recent entry
+        cf.chat_history.sort(key=lambda x: x['Time'])
         writer.writerow(cf.chat_history[-1].values())
+    
     cf.script_check += 1
 
-
-# ASR function starts
+# =======================================================================
+# ASR Function
+# =======================================================================
 class listen_micr:
+    # Configuration used in respond_to_user_utt()
+    last_X_chat_entries = 6
 
-    def __init__(self):
+    # Initialize
+    def __init__(self): 
         self._running = True
 
     def terminate(self):
@@ -96,27 +109,48 @@ class listen_micr:
                     logger.info("Error details: {}".format(cancellation_details.error_details))
                 pass
 
+    # -----------------------------------------------------------------------
+    # Response Selection
+    # -----------------------------------------------------------------------
+    # Helper function for formatting each turn in the chat history for LLM input
+    def format_turn(self, turn):
+        speaker = "user" if (turn["Speaker"] == "User") else "assistant"
+        return f"\n<|{speaker}|>\n{turn['Utt']}<|end|>"
+    
+    # Helper function to prepare the LLM input text
+    # (uses a set amount of the chat history to give context to the LLM)
+    def prepare_LLM_input(self, user_utt, chat_history):
+        # Only use the last X entries of the chat_history
+        history = chat_history[-self.last_X_chat_entries:]
+        logger.info("history: " + str(history))
+        
+        # Start the LLM input string with the specified prompt from configuration
+        LLM_input = f"<|system|>\n{cf.prompt}<|end|>"
+
+        # Format the each turn in the history for LLM input & add them to the LLM input string
+        LLM_input += "".join([self.format_turn(turn) for turn in history])
+
+        # Finally, to complete the LLM input, add the most recent user's utterance & a tag for the LLM to respond            
+        LLM_input += f"\n<|user|>\n{user_utt}<|end|>\n<|assistant|>\n"
+        logger.info("input_text: " + LLM_input)
+
+        # Return the completed prompt
+        return LLM_input
+
     # Response selection based on the ASR result
     def respond_to_user_utt(self, user_utt, chat_history):
         try:
-            history = []
-            for i in range(max(0, len(chat_history) - 6), len(chat_history)):
-                history.append(chat_history[i])
-
-            logger.info("history: " + str(history))
-            input_text = f"<|system|>\n{cf.prompt}<|end|>"
-            for turn in history:
-                if turn['Speaker'] == 'User':
-                    input_text += f"\n<|user|>\n{turn['Utt']}<|end|>"
-                else:
-                    input_text += f"\n<|assistant|>\n{turn['Utt']}<|end|>"
-            input_text += f"\n<|user|>\n{user_utt}<|end|>\n<|assistant|>\n"
-            logger.info("input_text: " + input_text)
+            # Get an input string to give the LLM
+            LLM_input = self.prepare_LLM_input(user_utt, chat_history)
             
-            output = cf.llm(input_text, max_tokens=cf.max_length, stop=["<|end|>", ".", "?"], echo=True)
-
+            # Get output text from the LLM
+            output = cf.llm(LLM_input, max_tokens=cf.max_length, stop=["<|end|>", ".", "?"], echo=True)
             system_utt = (output['choices'][0]['text'].split("<|assistant|>")[-1]).strip()
-            tts.synthesize_utt(system_utt)
+
+            # Synthesize the utterance into audio output (text-to-speech)
+            if cf.USE_CLOUD:
+                tts.synthesize_utt(system_utt)
+
         # If error occurs, write down the error at the logs/dm.log file
         except Exception as err:
             logger.error("user input parsing failed " + str(err))
