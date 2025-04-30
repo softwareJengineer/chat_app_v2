c5742eeeb60ebd65dd4741c04dc8425658e441a6
amurphy99 <amurphy62299@gmail.com>
2025-04-29 19:09:06 -0500

    New audio streaming
    Some of the old stuff was deprecated. This should also hopefully make it easier to implement the Google ASR stuff.

---
+++ frontend/public/audio-worklet-raw.js
@@ -0,0 +1,11 @@
+/* audio-worklet-raw.js
+ * Pushes raw Float32 PCM frames (128 samples by default) to the main thread.
+ */
+class RawAudioProcessor extends AudioWorkletProcessor {
+    process(inputs) {
+      // inputs[0][0] is a Float32Array for channel-0 of the mic
+      if (inputs[0] && inputs[0][0]) {this.port.postMessage(inputs[0][0]);} // transfer one frame
+      return true;                                                          // keep running
+    }
+  }
+  registerProcessor('raw-audio-processor', RawAudioProcessor);  
