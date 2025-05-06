dd37f2f45f181965dfd6aa1b024e83306d9e4f1f
Diego Izaguirre <tenochca.di@outlook.com>
2025-05-06 01:21:27 -0500

    fixed icon format, added second icon

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()] 
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'AI Assistant Chat with Biomarkers',
+        short_name: 'AI Chat',
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#ffde59',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            "src": "3d-icon-robot-1.png",
+            "sizes": "192x192",
+            "type": "image/png"
+        },
+        {
+            "src": "3d-icon-robot-2.png", 
+            "sizes": "512x512",
+            "type": "image/png"
+        }
+        ]
+      }
+    })
+  ]
 })
