dc2ba2201521b4ccd6412b5465333fc8a5a7a0fe
amurphy99 <amurphy62299@gmail.com>
2025-05-14 16:42:53 -0500

    New docker frontend/backend setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()] 
+  plugins: [react(), tailwindcss()],
+  server: {proxy: {'/ws': {target: 'http://backend:8000', ws: true}}},
 })
