e1e1d864645c624648b40ac0c3c003c17d541156
amurphy99 <amurphy62299@gmail.com>
2025-05-15 15:30:52 -0500

    ngrok

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {proxy: {'/ws': {target: 'http://backend:8000', ws: true}}},
+    plugins: [react(), tailwindcss()],
+    server: {
+        host: '0.0.0.0',      // listen on all interfaces (not just localhost)
+        port: 5173,
+        strictPort: true,     // make sure it uses 5173
+        cors: true,           // allow cross-origin access if needed
+        proxy: {
+        '/ws': {
+            target: 'http://backend:8000',
+            ws: true,
+        },
+        },
+    },
 })
