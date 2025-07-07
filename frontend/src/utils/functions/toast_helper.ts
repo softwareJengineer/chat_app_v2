7842d0c4bac10df63e311b56031a95bd75712a21
amurphy99 <amurphy62299@gmail.com>
2025-07-07 15:58:48 -0500

    utility stuff

--- frontend/src/utils/functions/toast_helper.ts
+++ frontend/src/utils/functions/toast_helper.ts
@@ -2,7 +2,7 @@ import toast from "react-hot-toast";
 
 
 export function toastMessage (text: string, success: boolean) {
-    if (success) { console.log (text); toast.success(text); }
+    if (success) { console.info(text); toast.success(text); }
     else         { console.warn(text); toast.error  (text); }
 };
 
--- frontend/src/utils/styling/sharedStyles.ts
+++ frontend/src/utils/styling/sharedStyles.ts
@@ -1,6 +1,13 @@
 
 export const h2 = "text-3xl align-middle fw-bold";
 export const h3 = "text-2xl align-middle fw-bold";
+export const h4 = "text-xl  align-middle fw-bold mb-1";  // (used for individual form headers)
 
 export const cardStyle = "border-1 border-gray-300 rounded p-[2rem] hover:shadow-xl h-full w-full justify-self-start";
 export const buttStyle = "bg-violet-600 rounded p-2 text-white";
+
+// --------------------------------------------------------------------
+// Form Style
+// --------------------------------------------------------------------
+export const switchStyle = "form-check form-switch w-100";
+export const switchLabel = "font-medium";
