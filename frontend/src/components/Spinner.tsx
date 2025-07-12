8a42b7b86aae7bc07243bda82dfc603e094f8445
amurphy99 <amurphy62299@gmail.com>
2025-07-12 11:37:23 -0500

    make spinner larger

--- frontend/src/components/Spinner.tsx
+++ frontend/src/components/Spinner.tsx
@@ -1,5 +1,5 @@
 export const Spinner = () => (
     <div className="flex items-center justify-center p-4">
-        <div className="h-5 w-5 animate-spin rounded-full border-2 border-gray-300 border-t-transparent" />
+        <div className="h-25 w-25 animate-spin rounded-full border-2 border-gray-300 border-t-transparent" />
     </div>
 );
