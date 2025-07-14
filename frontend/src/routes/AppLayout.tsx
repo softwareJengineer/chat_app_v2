a19247ded23ed5aac04bc50bf8097ed19b7b8616
amurphy99 <amurphy62299@gmail.com>
2025-07-14 14:43:40 -0500

    Update AppLayout.tsx

--- frontend/src/routes/AppLayout.tsx
+++ frontend/src/routes/AppLayout.tsx
@@ -10,9 +10,15 @@ export function AppLayout() {
     const pageHeader = (user            ) ? (<Header/>) : null;
     const DevBar     = (RUN_ENV == "DEV") ? (
         <div className="bg-yellow-100 px-4 py-1 text-xs flex gap-4">
-            <span>user: {user?.username ?? "—"}</span>
-            <span>role: {profile?.role  ?? "—"}</span>
+            
             <span>profile loaded: {profile ? "yes" : "no"}</span>
+            <span>user loaded: {user ? "yes" : "no"}</span>
+            <div className="vr"></div>
+
+            <span>user: {user?.username ?? "—"}</span>
+            <span>role: {profile?.role  ?? "—"}</span>     
+            <span>is_staff: {user?.is_staff ? "yes" : "no"}</span>
+
         </div>
     ) : null;
 
