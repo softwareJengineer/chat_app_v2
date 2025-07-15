d58333033f7f9636f40ce2963683aa5b51ab6dda
amurphy99 <amurphy62299@gmail.com>
2025-07-15 13:09:04 -0500

    finally works

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -8,13 +8,15 @@ envsubst '${DOMAIN} ${DOMAIN_WWW}' \
 
 # First-run: obtain cert if missing
 if [ ! -f "/etc/letsencrypt/live/${DOMAIN}/fullchain.pem" ]; then
-    certbot certonly --standalone --non-interactive --agree-tos \
-        -d "${DOMAIN}" -d "${DOMAIN_WWW}" \
-        --email "${CERT_EMAIL}"
+  certbot certonly --webroot \
+    --webroot-path /var/www/certbot \
+    --non-interactive --agree-tos \
+    -d "${DOMAIN}" -d "${DOMAIN_WWW}" \
+    --email "${CERT_EMAIL}"
 fi
 
 # Cron for renewals
-echo "0 3 * * * certbot renew --quiet --post-hook 'nginx -s reload'" | crontab -
+echo "0 3 * * * certbot renew --webroot -w /var/www/certbot --quiet --post-hook 'nginx -s reload'" | crontab -
 crond
 
 # Exec nginx in foreground
